be5c0642df1128515850da91519bfa411ba42228
Syed <syed@fahmpartners.com>
2017-04-11 10:14:09 +0800

    Added the search functionality

--- src/com/training/dao/DealDao.java
+++ src/com/training/dao/DealDao.java
@@ -1,13 +1,18 @@
 package com.training.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
 
 import com.training.model.Deal;
 
 public class DealDao {
 
 	private static List<Deal> DEALS = new ArrayList<Deal>();
+	private static Map<Integer, Deal> DEALS_MAP = new HashMap<Integer, Deal>();
 	
 	private static final DealDao INSTANCE = new DealDao();
 	private static Integer SEQUENCE = 0;
@@ -21,6 +26,16 @@ public class DealDao {
 	public void addDeal(Deal deal) {
 		deal.setId(++SEQUENCE);
 		DEALS.add(deal);
+	}
+
+	public Deal searchDeal(int id) {
+		if(MapUtils.isNotEmpty(DEALS_MAP)) {
+			if(DEALS_MAP.containsKey(id)) {
+				return DEALS_MAP.get(id);
+			}
+		}
+		
+		return null;
 	}
 
 }
